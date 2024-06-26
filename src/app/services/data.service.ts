4615339ffe1af44b4df14654fdcf9f3f5246b330
Ferdy Fauzi <payen83@gmail.com>
2024-06-26 12:42:36 +0800

    data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  private _storage: Storage;
+  constructor(private storage: Storage) { 
+    this.init();
+  }
 
-  constructor() { }
+  async init(){
+    const storage = await this.storage.create();
+    this._storage = storage;
+  }
+
+  async saveData(key: any, value: any){
+    await this._storage.set(key, value);
+  }
+
+  async getData(key: string){
+    return await this._storage.get(key);
+  }
 }
