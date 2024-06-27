7297e6f8bf9a1a51c395d8f4440f00a1a2fee5dc
Ferdy Fauzi <payen83@gmail.com>
2024-06-27 13:33:46 +0800

    camera

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'UTMSpaceApp',
+  appId: 'com.utmspace.app',
+  appName: 'UTM Space',
   webDir: 'www'
 };
 
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { APIService } from 'src/app/services/api.service';
 import { DataService } from 'src/app/services/data.service';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-login',
@@ -10,11 +11,27 @@ import { DataService } from 'src/app/services/data.service';
 })
 export class LoginPage implements OnInit {
   public user: any = { email: 'test1@email.com', password: 'test1234' };
+  public imagePath: any = null;
+
   constructor(
     private api: APIService, 
     private router: Router,
     private data: DataService
   ) { }
+
+  async takePhoto(){
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri
+    });
+  
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    this.imagePath = image.webPath;
+  }
 
   ngOnInit() {
   }
